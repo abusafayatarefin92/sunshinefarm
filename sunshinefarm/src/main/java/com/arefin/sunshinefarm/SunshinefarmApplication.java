abb64a34b04d97596a8f1406c4647bcfae4347b6
Safayat Arefin <abusafayatarefin92@gmail.com>
2018-12-20 14:12:03 +0600

    sun

---
+++ sunshinefarm/src/main/java/com/arefin/sunshinefarm/SunshinefarmApplication.java
@@ -0,0 +1,14 @@
+package com.arefin.sunshinefarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunshinefarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunshinefarmApplication.class, args);
+    }
+
+}
+
---
+++ sunshinefarm/src/test/java/com/arefin/sunshinefarm/SunshinefarmApplicationTests.java
@@ -0,0 +1,17 @@
+package com.arefin.sunshinefarm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SunshinefarmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
